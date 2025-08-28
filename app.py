3767ec04e8cf2ab35f2f6fbc75cc9c0b32254851
diegopena557 <dipenaro@gmail.com>
2025-08-28 12:49:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.title(" Hola buenas tardes, alguien sabe como llegar a loquendo city ")
 st.header ("Hola alguno sabe programar C++")
 
 image = Image.open('Waltuh.jpg')
+
+st.image(image, caption = 'Interfaces multimodales')
