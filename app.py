a76a61c86eb958e8136357fd5b4f5742423cf1e9
diegopena557 <dipenaro@gmail.com>
2025-08-28 12:46:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import PIL import Image
 
+st.title(" Hola buenas tardes, alguien sabe como llegar a loquendo city ")
 
-st.title(" Mi primera APP!! ")
+st.header ("Hola alguno sabe programar C++")
+
+image = Image.open ('Waltuh.jpg')
